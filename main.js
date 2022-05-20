ce9a4d02991eb7603d6e9000b8d6a41f9cac5d79
Poorvi1010 <91541629+Poorvi1010@users.noreply.github.com>
2022-05-20 17:49:38 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,31 @@ function perload()
 {
 
 }
+
 function setup()
 {
     canvas=createCanvas(450,450);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
+
+function modelLoaded()
+{
+    console.log("Model Loaded");
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+    }
+}
+
 function draw()
 {
-    
+    background('#f5ee1b')
 }
